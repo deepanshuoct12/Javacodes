fad89518c6537e14d35ce697534a77329b8f2f2d
deepanshuoct12 <deepanshu.oct12@gmail.com>
2022-03-27 14:04:42 +0530

    first demo project

---
+++ demo/src/main/java/com/example/demo/data/Guest.java
@@ -0,0 +1,106 @@
+package com.example.demo.data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "GUEST")
+public class Guest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "GUEST_ID")
+    private int guest_id;
+    @Column(name = "FIRST_NAME")
+    private String first_name;
+    @Column(name = "LAST_NAME")
+    private String last_name;
+    @Column(name = "EMAIL_ADDRESS")
+   private String email_address;
+    @Column(name = "ADDRESS")
+    private String addres;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "STATE")
+    private String state;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "guest_id=" + guest_id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email_address='" + email_address + '\'' +
+                ", addres='" + addres + '\'' +
+                ", country='" + country + '\'' +
+                ", state='" + state + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                '}';
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public int getGuest_id() {
+        return guest_id;
+    }
+
+    public void setGuest_id(int guest_id) {
+        this.guest_id = guest_id;
+    }
+
+    @Column(name = "PHONE_NUMBER")
+    private String phone_number;
+
+}
